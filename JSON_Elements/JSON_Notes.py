2686e957f54a048c79688fba2f97b38653bc9313
ryanbeard82 <ryan_o_beard@me.com>
2022-01-31 22:05:26 -0600

    outline of MIDI note capture code

--- JSON_Elements/JSON_Notes.py
+++ JSON_Elements/JSON_Notes.py
@@ -2,7 +2,7 @@ class JSON_Notes():
     # This class stores the 3 expected JSON note parameters
     #intended to be declared as list variables, one for each wave channel
     
-    def __init__ (self):
-        self.value
-        self.octave
-        self.length
+    def __init__ (self,value,octave,length):
+        self.value = value
+        self.octave = octave
+        self.length = length
