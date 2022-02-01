0fc7013b65707632420d99550891c124f181c1d8
ryanbeard82 <ryan_o_beard@me.com>
2022-01-31 22:06:47 -0600

    outline of MIDI note capture code

--- MIDI to JSON.py
+++ MIDI to JSON.py
@@ -2,22 +2,27 @@ from ast import Constant
 from fileinput import filename
 from itertools import count
 from operator import countOf
+from telnetlib import theNULL
 from turtle import title
+from unittest import case
 from mido import MidiFile
 from tkinter import *
 from pathlib import Path
 from tkinter.filedialog import askopenfilename, asksaveasfile, askdirectory
-from JSON_Elements import *
 from pick import pick
 import logging
+from JSON_Elements.JSON_Songs import JSON_Songs
+from JSON_Elements.JSON_Notes import JSON_Notes
 from MIDI_Elements.MIDI_Track import MIDI_Track
 import os
+import sys
 
 logging.basicConfig(level=logging.DEBUG)
 
 # Set constants & Global Variables
 DEV_MODE = True
 SYNTH_CHANNELS = ["Sine","Square","Sawtooth","Triangle","Noise","Ignore Track"]
+MAX_NOTES = 512
 
 def main():
     # main subroutine
@@ -45,8 +50,10 @@ def main():
     
     logging.info("Selected MIDI Filename: " + yourMidiFile.filename)
     
-    # get bpm info
+    # get bpm and clock info
     songBPM = round(yourMidiFile.ticks_per_beat/6)
+    ticksPerBeat = yourMidiFile.ticks_per_beat
+    sixteenthNoteDeafalt = ticksPerBeat/4
     logging.info("MIDI Ticks per beat: " + str(yourMidiFile.ticks_per_beat))
     logging.info("MIDI BPM: " + str(songBPM))
     
@@ -54,15 +61,71 @@ def main():
     midiTracks = getTracks(yourMidiFile)
     
     # assign pulp synth channels
-    getTrackMapping(midiTracks)
+    tracksAssigned = getTrackMapping(midiTracks)
     
+    # exit if the user did not assign any tracks
+    if not tracksAssigned:
+        sys.exit("No tracks assigned. Exiting application.")
+    
+    # create new song if the user has assigned track maps
+    songName = input("Enter your song's name:")
+    userSong = JSON_Songs(0,songBPM,songName)
+    
+    # evaluate notes for each track and assign to song
     for track in midiTracks:
         print(str(track.track_number) + " - " + track.track_name + " is mapped to " + track.synth_channel)
+        if track.synth_channel == "Sine":
+            return
+        elif track.synth_channel == "Square":
+            return
+        elif track.synth_channel == "Sawtooth":
+            return
+        elif track.synth_channel == "Triangle":
+            return
+        elif track.synth_channel == "Noise":
+            return
+            
+    sys.exit("Exiting application.")
+        
+def getNotes(midiTrack: MIDI_Track) -> JSON_Notes:
     
-def getTrackMapping(midi_tracks: MIDI_Track) -> MIDI_Track:
+    convertedMIDINotes = []
+    noteCounter = 0
+    deltaTime = 0
+    lastNote = 0
+
+    for msg in midiTrack:
+        if not msg.is_meta:
+            noteCounter += 1
+            if msg.type == "note_on":
+                if msg.time != 0:
+                    # TODO: if the note on msg time is not 0, add new JSON null notes to fill 16th note spaces
+                    return
+            elif msg.type == "note_off":
+                noteValue = getNoteValue(msg.note)
+                noteOctave = getNoteOctave(msg.note)
+                noteLen = getNoteLength(msg.note)
+                newJSONnote = JSON_Notes()
+    
+    return convertedMIDINotes
+
+def getNoteValue():
+    # TODO: need code to convert MIDI note to JSON note value
+    return
+
+def getNoteOctave():
+    # TODO: need code to get JSON note octave from MIDI note value
+    return
+    
+def getNoteLength():
+    # TODO: need code to get JSON note length (in 16ths) from MIDI note time
+    return
+
+def getTrackMapping(midi_tracks: MIDI_Track) -> bool:
     # use "pick" to collect MIDI track mapping
     # to Pulp synth channels from the user via Terminal
     
+    trackValidator = False
     availableSynthList = SYNTH_CHANNELS
     trackNumber = 0
     print("You must map each MIDI track to one of Pulp's 5 synth engines.")
@@ -74,10 +137,13 @@ def getTrackMapping(midi_tracks: MIDI_Track) -> MIDI_Track:
         logging.info("User selected " + selectedSynth + " for Track " + str(midi_tracks[trackNumber].track_number) + " - " + midi_tracks[trackNumber].track_name)
         
         if selectedSynth != "Ignore Track":
+            trackValidator = True
             midi_tracks[trackNumber].synth_channel = selectedSynth
             synthOptions.remove(selectedSynth)
         
         trackNumber += 1
+    
+    return trackValidator
     
 def getTracks(midi_file: MidiFile) -> MIDI_Track:
     # collects track data from MIDI file
